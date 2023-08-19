e0d6e274c50271a6b84800764377e703123e7344
ajaykrishnan07 <ajkrish2002@gmail.com>
2023-08-20 01:27:50 +0530

    Issues
    Still have issues integrating tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./assets/main.css"
 
 function App() {
   
   return (
-   <>Hello World!</>
+   <div>
+    <h1 className='text-6xl'>Hello World!</h1>
+
+   </div>
   )
 }
 
