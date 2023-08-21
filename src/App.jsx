503afd5c15b9500529925e6e2c4590c079598813
ajaykrishnan07 <ajkrish2002@gmail.com>
2023-08-21 23:59:06 +0530

    fixed Tailwind Integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
-import "./assets/main.css"
+import { useState } from "react";
+import "./assets/main.css";
 
 function App() {
-  
   return (
-   <div>
-    <h1 className='text-6xl'>Hello World!</h1>
-
-   </div>
-  )
+    <div>
+      <h1 className="text-lime-600 text-7xl hover:text-violet-900 hover:text-sm">
+        Hello World!
+      </h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-   "./src/App.jsx",
-   "./tailwind.js"
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {},
